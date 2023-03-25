9d44721b663899f3c4c0abf621a96be706d0d05c
Estate Lelashvili <estatelelashvili@gmail.com>
2023-03-25 18:48:54 +0400

    feat: add DbConnect class

---
+++ backend/php/000webhost/includes/connection/DbConnect.php
@@ -0,0 +1,22 @@
+<?php
+
+class DbConnect{
+    private $host = "localhost";
+    private $username = "id20372848_estate";
+    private $password = "#TRYharderfish2019";
+    private $database = "id20372848_db_products";
+    public $conn;
+    
+    public function __construct() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+    
+    public function getConnection() {
+        return $this->conn;
+    }    
+}
+
+?>
