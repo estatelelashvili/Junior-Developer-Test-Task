4207f399db218c4d170899298dacb3cc1480edcb
Estate Lelashvili <estatelelashvili@gmail.com>
2023-03-26 09:08:00 +0400

    feat: rename react-app

--- frontend/react-store/src/components/ProductList.js
+++ frontend/react-store/src/components/ProductList.js
@@ -9,13 +9,10 @@ class ProductList extends Component {
     this.state = {
       products: [],
       selectedProducts: [],
-      checkedStates: {},
     };
-    this.fetchProducts = this.fetchProducts.bind(this);
-    this.handleMassDelete = this.handleMassDelete.bind(this);
   }
 
-  async handleMassDelete() {
+  handleMassDelete = async () => {
     const { selectedProducts } = this.state;
     const deletePromises = selectedProducts.map((product) =>
       this.handleDeleteProduct(product)
@@ -27,30 +24,28 @@ class ProductList extends Component {
       console.error('There was a problem with the fetch operation:', error);
     }
     this.setState({ selectedProducts: [] });
-    // this.uncheckAllCheckboxes();
-  }
+  };
 
-  async handleDeleteProduct(product) {
+  handleDeleteProduct = async (product) => {
     product._METHOD = 'DELETE';
-    return fetch('/api/endpoint.php', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-HTTP-Method-Override': 'DELETE',
-      },
-      body: JSON.stringify(product),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-      })
-      .catch((error) => {
-        console.error('There was a problem with the fetch operation:', error);
+    try {
+      const response = await fetch('/api/endpoint.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-HTTP-Method-Override': 'DELETE',
+        },
+        body: JSON.stringify(product),
       });
-  }
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
 
-  async fetchProducts() {
+  fetchProducts = async () => {
     try {
       const response = await fetch('/api/endpoint.php');
       if (!response.ok) {
@@ -66,7 +61,7 @@ class ProductList extends Component {
       console.error(error);
       this.setState({ products: [] });
     }
-  }
+  };
 
   componentDidMount() {
     this.fetchProducts();
@@ -78,16 +73,8 @@ class ProductList extends Component {
     this.setState({ selectedProducts });
   };
 
-  uncheckAllCheckboxes() {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach((checkbox) => {
-      checkbox.checked = false;
-    });
-  }
-
   render() {
     const { products } = this.state;
-
     return (
       <div className='products-container'>
         <HeaderList onHandleMassDelete={this.handleMassDelete} />
@@ -98,9 +85,7 @@ class ProductList extends Component {
               <Product
                 key={index}
                 product={product}
-                handleFetchProducts={this.fetchProducts}
                 onHandleCheckBoxChange={this.handleCheckBoxChange}
-                onHandleDeleteProduct={this.handleDeleteProduct}
               />
             ))}
         </div>
