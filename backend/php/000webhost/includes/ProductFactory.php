092dc9dbc3aa99db2dc6c24d435f55a70dfabeec
Estate Lelashvili <estatelelashvili@gmail.com>
2023-03-26 04:45:44 +0400

    feat: add api class

--- backend/php/000webhost/includes/ProductFactory.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class ProductFactory {
-        public function create_product($product_type, $conn) {
-            $productClassName = ucfirst($product_type);
-            return new $productClassName($conn);
-        }
-    }
- ?>
