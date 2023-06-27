7b89cc3c7a364f351bdd7e503fcf4b66e93e34e6
Estate Lelashvili <estatelelashvili@gmail.com>
2023-06-27 15:45:48 +0400

    feat: remove app files

--- backend/php/InfinityFree/htdocs/includes/product_factory.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class ProductFactory {
-    public function createProduct($productType, $conn) {
-        $productClassName = ucfirst($productType);
-        return new $productClassName($conn);
-    }
-}
